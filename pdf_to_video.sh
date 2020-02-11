418971abfacea83562dbfa664a064d41357561f1
sc13nt1st <samuelwaweru1313@gmail.com>
2020-02-11 21:00:18 +0300

    Update pdf_to_video.sh

--- pdf_to_video.sh
+++ pdf_to_video.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # Convert pdf to png images
-pdftoppm -png ooc.pdf test
+# Will produce png files in the format test-001.png
+pdftoppm -png ooc.pdf test &&
 
 # Convert the png images into a video
-ffmpeg -i test-%03d.png video.mp4
+# %0n where n is the number of digits in the last page number. For example, if the last page is 234, then n = 3
+ffmpeg -i test-%03d.png video.mp4 &&
+
+# Delete the png files
+rm *.png
 
 # Play the video.mp4 file using MPV video player
 mpv video.mp4
